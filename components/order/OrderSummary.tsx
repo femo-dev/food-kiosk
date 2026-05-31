f6c9724ac1cfc52b7fdcef4076c57a2c7ecb2000
Fabian <fabianmendozaospina@outlook.com>
2026-05-31 06:41:16 -0500

    Zustand added for handling store

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,7 +1,18 @@
+"use client";
+import { useStore } from "@/src/store";
+
 export default function OrderSummary() {
+  const order = useStore((state) => state.order);
+  
   return (
     <aside className="md:h-screen md:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">My Order</h1>
+
+      {order.length === 0 ? <p className="text-center my-10">The cart is empty</p> : (
+        <div className="mt-5">
+          <p>S</p>
+        </div>
+      )}
     </aside>
   )
 }
