76cfdafce1504246d9735cf4c39ccf2b956e2254
Fabian <fabianmendozaospina@outlook.com>
2026-06-09 08:11:42 -0500

    ProductDetails.tsx improved

--- src/store.ts
+++ src/store.ts
@@ -7,16 +7,28 @@ interface Store {
   addToOrder: (product: Product) => void
 }
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set, get) => ({
   order: [],
   addToOrder: (product) => {
     const { categoryId, image, ...data } = product;
-    set((state) => ({ 
-      order: [...state.order, { 
+    let order : OrderItem[] = [];
+
+    if (get().order.find(item => item.id === product.id)) {
+      order = get().order.map(item => item.id === product.id ? {
+        ...item,
+        quantity: item.quantity + 1,
+        subtotal: (item.quantity + 1) * product.price
+      } : item)
+    } else {
+        order = [...get().order, { 
         ...data, 
         quantity: 1,
-        subtotal: product.price
+        subtotal: 1 * product.price
       }] 
+    }
+
+    set((state) => ({ 
+      order
     }));
   }
 }))
