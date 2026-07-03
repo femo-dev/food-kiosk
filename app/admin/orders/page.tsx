f982ce7de9c3963bd1c9b12677c2b6cf986b40fc
Fabian <fabianmendozaospina@outlook.com>
2026-07-03 07:58:13 -0500

    Logo and Heading components created

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,11 @@
+import Heading from "@/components/ui/Heading";
+
+export default function OrdersPage() {
+  return (
+    <>
+    <Heading>
+      Manage Orders
+    </Heading>
+    </>
+  )
+}
--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "@/components/products/ProductCard";
+import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
 async function getProducts(category: string) {
@@ -19,9 +20,9 @@ export default async function OrderPage({ params }: { params: Promise<{ category
   
   return (
     <>
-    <h1 className="text-2xl my-10">
-      Choose and customize your order below
-    </h1>
+      <Heading>
+        Choose and customize your order below
+      </Heading> 
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
         {products.map(product => (
           <ProductCard 
