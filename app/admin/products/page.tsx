7f2b4923fea3dee7533e9a024f842b753de255c6
Fabian <fabianmendozaospina@outlook.com>
2026-07-10 07:40:15 -0500

    Paginator initiated

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,8 +2,11 @@ import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
-async function getProducts(){
+async function getProducts(page: number, pageSize: number){
+  const skip = (page - 1) * pageSize;
   const products = await prisma.product.findMany({
+    take: pageSize,
+    skip,
     include: {
       category: true
     }
@@ -14,8 +17,10 @@ async function getProducts(){
 
 export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
 
-export default async function ProductsPage() {
-  const products = await getProducts();
+export default async function ProductsPage({searchParams}: { searchParams: { page: string }}) {
+  const page = parseInt(searchParams.page) || 1;
+  const pageSize = 10;
+  const products = await getProducts(page, pageSize);
 
   return (
     <>
